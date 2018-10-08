64fd26592b1292e584391d775bda3cf66d14b72a
Ahnaf Shahriar <abir3577189@gmail.com>
2018-10-09 03:06:13 +1100

    Bucket added

--- second.php
+++ second.php
@@ -4,21 +4,23 @@
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Weather today</title>
-	<meta charset='UTF-8'>
-	
-	<link href='https://fonts.googleapis.com/css?family=Cabin' rel='stylesheet' type='text/css'>
-	<link rel='stylesheet' type='text/css' href='/css/style.css'>
-	
-</head>
+ <head>
+      <title>Weather Today</title>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+     <link rel="stylesheet" type="text/css" href="css/style.css">
+  </head>
 <body>
+	 <?php include("nav.php") ?>
+      <div class="container">
 	<div id='header'>
-		Weather for <?php echo htmlspecialchars($_GET['city'])?> is: 
-		<p><a href="./index.php">Go back</a></p>
+		<h1>Weather for <?php echo htmlspecialchars($_GET['city'])?> is: </h1>
 	</div>
 	
-	<div class='content'>
 	<?php
 		$client = new Google_Client();
 		$client->useApplicationDefaultCredentials();
@@ -36,14 +38,15 @@
 		$year = htmlspecialchars($_GET['year']);
 
 		$request->setQuery("SELECT stn, year, mo, da, max, min 
-FROM [bigquery-public-data:noaa_gsod.gsod2018]
-where stn ='996470' and year ='$year'and mo='0$month' and da='$date'");
+				FROM [bigquery-public-data:noaa_gsod.gsod2018]
+				where stn ='996470' and year ='$year'and mo='0$month' and da='$date'");
 		
 		$response = $bigquery->jobs->query($projectId, $request);
 		$rows = $response->getRows();
 
 		$str = "<table>".
 		"<tr>" .
+		"<th>State Name</th>" .
 		"<th>Station</th>" .
 		"<th>Year</th>" .
 		"<th>Month</th>" .
@@ -66,7 +69,18 @@ where stn ='996470' and year ='$year'and mo='0$month' and da='$date'");
 		$str .= '</table></div>';
 
 		echo $str;
+
+	 	$handle = fopen('gs://project-1558.appspot.com/cloud_assign2/searches'.date("Y-m-d h:i:sa").'.txt','w');
+
+	 	fwrite($handle, "User searched at ".date("Y-m-d h:i:sa")." ------ "."City : ".$city.", Date : ".$date.", Month- ".$month.", Year: ".$year);
+
+		
+
+		fclose($handle);
+		
+
 	?>
-	</div>
+	
+</div>
 </body>
 </html>
